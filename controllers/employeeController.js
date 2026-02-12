ab82ac9d8c8a2eb4c91e5ed8d27aa218572ca65e
unknown <vishakvijayan2002@gmail.com>
2026-02-12 11:21:44 +0530

    employeeServer v2

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -25,3 +25,55 @@ exports.addEmployeeController=async(req,res)=>{
     }
     
 }
+
+exports.fetchEmployeeController=async(req,res)=>{
+    console.log("Inside fetchEmployeeController");
+    try
+    {
+        const AllEmployees=await employees.find()
+        res.status(200).json(AllEmployees)
+    }
+    catch(err)
+    {
+        res.status(401).json(err)
+    }    
+}
+
+exports.deleteEmployeeController=async(req,res)=>{
+    console.log("Inside deleteEmployeeController");
+    const {id} = req.params
+    try
+    {
+        const deleteEmployee = await employees.findByIdAndDelete(id)
+        if(deleteEmployee)
+        {
+            res.status(200).json("Employee deleted successfully")
+        }
+        else
+        {
+            res.status(404).json("Employee not found")
+        }
+    }
+    catch(err)
+    {
+        res.status(401).json(err)
+    }
+
+}
+
+exports.updateEmployeeController=async(req,res)=>{
+    console.log("Inside updateEmployeeController");
+    const {id}=req.params
+    const{name,email,designation,salary}=req.body
+    try
+    {
+        const updateEmployee=await employees.findByIdAndUpdate(id,{
+            name,email,designation,salary
+        },{new:true})
+        res.status(200).json(updateEmployee)
+    }
+    catch(err)
+    {
+        res.status(401).json(err)
+    }  
+}
--- routes/router.js
+++ routes/router.js
@@ -5,5 +5,9 @@ const employeeController=require('../controllers/employeeController')
 const router=express.Router()
 
 router.post('/addEmployee',employeeController.addEmployeeController)
+router.get('/getEmployee',employeeController.fetchEmployeeController)
+router.delete('/deleteEmployee/:id',employeeController.deleteEmployeeController)
+router.put('/updateEmployee/:id',employeeController.updateEmployeeController)
+
 
 module.exports=router
